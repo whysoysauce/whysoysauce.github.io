e9f4729473a1f27b761be56e8a77d0e7408a6287
Lian Liu <lianliu@Lians-MacBook-Pro.local>
2024-11-16 21:09:44 -0800

    Modify App.js and add test pic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import p3r from "./assets/images/p3r.jpg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={p3r} alt="Test pic"/>
+        </div>
       </header>
     </div>
   );
